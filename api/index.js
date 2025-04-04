8af38f5c2e26902259bc988201fbe2ca3562e40a
Achyut Gupta <achyut10gupta@gmail.com>
2025-04-04 19:53:28 +0530

    Databse connection and header setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
 
 const app = express();
-
-app.listen(3000, () => {
-    console.log("App is listening on port 3000!!")
+import dotenv from "dotenv";
+dotenv.config({
+    path : "./.env"
 })
+import connectDB from './db/index.js';
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongo DB Connection error", err)
+})
+
+
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import About from './Pages/About';
+import Home from './Pages/Home';
+import Profile from './Pages/Profile';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Header from './assets/Components/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
