ff4d09567f30609a2cef125b53066797ab80d5f2
Achyut Gupta <achyut10gupta@gmail.com>
2025-06-02 21:43:19 +0530

    error fix

--- api/index.js
+++ api/index.js
@@ -8,9 +8,12 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 3000, () => {
+        const server = app.listen(process.env.PORT || 3000, () => {
             console.log(`Server is running at PORT : ${process.env.PORT}`);
         });
+
+        server.keepAliveTimeout = 120000;
+        server.headersTimeout = 120000;
     })
     .catch((err) => {
         console.log("Mongo DB Connection error", err);
