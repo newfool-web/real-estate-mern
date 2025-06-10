c30b0d52b5a0b9bf99d1ce6002934cdd9921a840
Achyut Gupta <achyut10gupta@gmail.com>
2025-06-11 01:19:59 +0530

    add vercel url

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: process.env.NODE_ENV === 'production'
-          ? '' 
+          ? 'https://estate-link-two.vercel.app/' 
           : 'http://localhost:3000/',
         secure: false,
       },
