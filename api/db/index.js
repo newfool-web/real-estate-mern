0d876ee346222d1dc1dfd916c86f27501fc7e769
Achyut Gupta <achyut10gupta@gmail.com>
2025-06-12 17:18:35 +0530

    error fix

--- api/db/index.js
+++ api/db/index.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {    
-    
-    try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
-    }
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export default connectDB
+const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      // Add your options here if needed
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default connectDB;
